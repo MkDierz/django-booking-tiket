0fb6a2186b95a0e48658903b1fa77b46028d96f5
MkDierz <msyibbran24@gmail.com>
2022-06-28 18:50:41 +0700

    update script

--- run.bat
+++ run.bat
@@ -1,13 +1,4 @@
-if exist env\ (
-  env\Script\activate
-) else (
-  pip install virtualenv
-  virtualenv env
-  env\Script\activate
-  pip install -r requirements.txt
-)
-
-
+pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
 python manage.py loaddata data.json
